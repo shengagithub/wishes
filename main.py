62c95669cf219a009344ccbc2fba319d041db3b8
shengagithub <804387088@qq.com>
2022-06-06 17:21:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from PIL import Image
-st.write("希望胡面面开心快乐每一天，每天都有惊喜！")
+st.set_page_config(page_title="宋梦装逼专用网站",    #页面标题
+    page_icon=":rainbow:",        #icon
+    layout="wide",                #页面布局
+    initial_sidebar_state="auto"  )
+st.write("希望梦公子越长越帅！")
 
 image = Image.open('2.jpg')
 st.image(image, caption='Sunrise by the mountains',use_column_width=True)
