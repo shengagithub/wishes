a0a2fbf440b9ad8f49b493906eac2850d98ad2c7
shengagithub <804387088@qq.com>
2022-06-06 10:53:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
-st.write("希望我的吴珊媚开心快乐每一天，每天都有惊喜！")
+st.write("希望胡面面开心快乐每一天，每天都有惊喜！")
 
-image = Image.open('1.jpg')
+image = Image.open('2.jpg')
 st.image(image, caption='Sunrise by the mountains',use_column_width=True)
